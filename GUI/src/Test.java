140e766cec3446925913061413e3628e04ba16a0
Hubert12-12 <eakan699@gmail.com>
2021-10-20 20:43:55 +0200

    GUI erstellt ohne Button

--- GUI/src/Test.java
+++ GUI/src/Test.java
@@ -1,9 +1,14 @@
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 
 public class Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println("ndaid"); 
+	new GUI();
+		
+		
 	}
 } 
 
