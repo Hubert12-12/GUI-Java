975c1504c0e35063bd93e9607eb73c816ae5a4b5
Hubert12-12 <eakan699@gmail.com>
2021-10-20 19:06:15 +0200

    Neu Anfang

--- GUI/src/Test.java
+++ GUI/src/Test.java
@@ -3,16 +3,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello wfaawfaf");
-		A();
-	}
-	
-	public static void A() {
-		
-		while (true) {
-	
-		System.out.println("d");
 	}
 }
-	}
 
